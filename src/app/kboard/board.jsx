d5226ed347adb1e4e6404bc6d3ec98e7f024a46b
Leslie Su <leslie.su@outlook.co.nz>
2021-10-09 01:14:16 +1300

    style layer on stage through Rect shape

--- src/app/kboard/board.jsx
+++ src/app/kboard/board.jsx
@@ -1,87 +1,35 @@
 import React from 'react';
 import { makeStyles } from '@mui/styles';
-import { Stage, Layer, Star, Text  } from 'react-konva';
-import GridNineSVG from 'images/kboard/plus.svg';
+import { Stage, Layer, Rect  } from 'react-konva';
+import PlusSVG from 'images/kboard/plus.svg';
 
 const useStyles = makeStyles({
-  root: {
+  stage: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    // background: `url(${GridNineSVG}) top left`,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    // background: `url(${PlusSVG}) top left`,
     marginLeft: 'auto',
     marginRight: 'auto',
     left: 0,
     right: 0,
     width: (props) => `${props.width}px`,
     height: (props) => `${props.height}px`,
-  },
+  }
 });
 
-function generateShapes() {
-  return [...Array(10)].map((_, i) => ({
-    id: i.toString(),
-    x: Math.random() * window.innerWidth,
-    y: Math.random() * window.innerHeight,
-    rotation: Math.random() * 180,
-    isDragging: false,
-  }));
-}
-
-const INITIAL_STATE = generateShapes();
 
 const Board = ({width, height}) => {
   const classes = useStyles({width, height});
-  const [stars, setStars] = React.useState(INITIAL_STATE);
 
-  const handleDragStart = (e) => {
-    const id = e.target.id();
-    setStars(
-      stars.map((star) => {
-        return {
-          ...star,
-          isDragging: star.id === id,
-        };
-      })
-    );
-  };
-  const handleDragEnd = (e) => {
-    setStars(
-      stars.map((star) => {
-        return {
-          ...star,
-          isDragging: false,
-        };
-      })
-    );
-  };
-
-  return  <Stage width={width} height={height} className={classes.root}>
+  return  <Stage width={width} height={height} className={classes.stage}>
     <Layer>
-        {stars.map((star) => (
-          <Star
-            key={star.id}
-            id={star.id}
-            x={star.x}
-            y={star.y}
-            numPoints={5}
-            innerRadius={20}
-            outerRadius={40}
-            fill="#89b717"
-            opacity={0.8}
-            draggable
-            rotation={star.rotation}
-            shadowColor="black"
-            shadowBlur={10}
-            shadowOpacity={0.6}
-            shadowOffsetX={star.isDragging ? 10 : 5}
-            shadowOffsetY={star.isDragging ? 10 : 5}
-            scaleX={star.isDragging ? 1.2 : 1}
-            scaleY={star.isDragging ? 1.2 : 1}
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-          />
-        ))}
-      </Layer>
+      <Rect
+        x={0}
+        y={0}
+        width={width}
+        height={height}
+        fill="#ddd"
+      />
+    </Layer>
   </Stage>
 }
 
